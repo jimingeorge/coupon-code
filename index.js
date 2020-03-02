39e9424c63e2f49883511a7d15828ba06dd722af
Jimin George <jimingeorge101@gmail.com>
2020-03-02 18:55:38 +0530

    configured to deploy to heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3050
+const port = process.env.PORT || 5000;
 const router = require('./config/route')
 const setupDB = require('./config/database') 
 
